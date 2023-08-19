7cf2f1017b5cef2d59628159af861d56ac4d5111
youandvern <ayounguw@gmail.com>
2023-08-18 22:49:45 -0700

    update truss style selector for mobile formatting

--- src/components/TrussStyleSelector/index.tsx
+++ src/components/TrussStyleSelector/index.tsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { FormLabel, RadioGroup, FormControlLabel, Radio, Container } from "@mui/material/";
+import {
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  Container,
+  useMediaQuery,
+  Theme,
+} from "@mui/material/";
 import { grey } from "@mui/material/colors";
 
 interface TrussType {
@@ -7,16 +15,27 @@ interface TrussType {
   label: string;
 }
 
-export const TRUSS_TYPES = [
-  { type: "PrattBridgeTruss", label: "Pratt Bridge" },
+const ROOF_TRUSS_TYPES = [
   { type: "PrattRoofTruss", label: "Pratt Roof" },
-  { type: "HoweBridgeTruss", label: "Howe Bridge" },
   { type: "HoweRoofTruss", label: "Howe Roof" },
   { type: "FinkRoofTruss", label: "Fink Roof" },
+  { type: "ParallelChordRoofTruss", label: "Parallel Chord Roof" },
+];
+const BRIDGE_TRUSS_TYPES = [
+  { type: "PrattBridgeTruss", label: "Pratt Bridge" },
+  { type: "HoweBridgeTruss", label: "Howe Bridge" },
   { type: "WarrenBridgeTruss", label: "Warren Bridge" },
   { type: "ScissorTruss", label: "Scissor Truss" },
-  { type: "ParallelChordRoofTruss", label: "Parallel Chord Roof" },
-] as TrussType[];
+];
+
+const SMALL_SCREEN_TYPE_GROUPS = [
+  [BRIDGE_TRUSS_TYPES[0], ROOF_TRUSS_TYPES[0]],
+  [BRIDGE_TRUSS_TYPES[1], ROOF_TRUSS_TYPES[1]],
+  [BRIDGE_TRUSS_TYPES[2], ROOF_TRUSS_TYPES[2]],
+  [BRIDGE_TRUSS_TYPES[3], ROOF_TRUSS_TYPES[3]],
+];
+
+export const TRUSS_TYPES = [...BRIDGE_TRUSS_TYPES, ...ROOF_TRUSS_TYPES] as TrussType[];
 
 interface StyleSelectorProps {
   trussType: string;
@@ -25,8 +44,11 @@ interface StyleSelectorProps {
 
 // Radio buttons to select the truss style
 export default function TrussStyleSelector({ trussType, handleChange }: StyleSelectorProps) {
-  const roofTypes = TRUSS_TYPES.filter((typeName) => typeName.type.includes("Roof"));
-  const bridgeTypes = TRUSS_TYPES.filter((typeName) => !typeName.type.includes("Roof"));
+  const smallScreen = useMediaQuery((theme: Theme) => theme.breakpoints.down("sm"));
+  const typeGroupings = smallScreen
+    ? SMALL_SCREEN_TYPE_GROUPS
+    : [ROOF_TRUSS_TYPES, BRIDGE_TRUSS_TYPES];
+
   const groupedButtons = (typeList: TrussType[]) =>
     typeList.map((eachType) => (
       <FormControlLabel
@@ -58,12 +80,14 @@ export default function TrussStyleSelector({ trussType, handleChange }: StyleSel
         onChange={handleChange}
         aria-labelledby="choose-truss-style-buttons"
       >
-        <Container sx={{ display: "flex", justifyContent: "space-around" }}>
-          {groupedButtons(roofTypes)}
-        </Container>
-        <Container sx={{ display: "flex", justifyContent: "space-around" }}>
-          {groupedButtons(bridgeTypes)}
-        </Container>
+        {typeGroupings.map((trussTypeGroup, index) => (
+          <Container
+            key={`grouping-${index}`}
+            sx={{ display: "flex", justifyContent: "space-around" }}
+          >
+            {groupedButtons(trussTypeGroup)}
+          </Container>
+        ))}
       </RadioGroup>
     </Container>
   );
